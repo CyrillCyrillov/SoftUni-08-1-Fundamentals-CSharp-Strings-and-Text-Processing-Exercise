cf3aa1ed42052ba256daab27a83ad4b49b223086
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:37:17 +0200

    Add files via upload

---
+++ Task04_Caesar_Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task04_Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int asciiCode = input[i] + 3;
+                output += (char) asciiCode;
+            }
+            
+            
+            Console.WriteLine(output);
+        }
+    }
+}
