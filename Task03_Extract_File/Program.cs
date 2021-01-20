78b8d45ac7b6b4ae27b2c3c9fdfc3d6fead6c019
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:36:24 +0200

    Add files via upload

---
+++ Task03_Extract_File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Task03_Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] comandLine = Console.ReadLine().Split(new char[] { ':', '.', (char) 92 }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string fileName = comandLine[comandLine.Length - 2];
+            string extention = comandLine[comandLine.Length - 1];
+            
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extention}");
+        }
+    }
+}
