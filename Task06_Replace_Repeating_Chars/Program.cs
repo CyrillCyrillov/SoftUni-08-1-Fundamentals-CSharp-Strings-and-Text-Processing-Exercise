7f65574d2aa98bf22835019e8035b0dcd2c29e38
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:40:37 +0200

    Add files via upload

---
+++ Task06_Replace_Repeating_Chars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task06_Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int index = 0;
+
+            while (index < text.Length - 1)
+            {
+                if(text[index] == text[index +1])
+                {
+                    text = text.Remove(index + 1, 1);
+                }
+                else
+                {
+                    index += 1;
+                }
+            }
+            
+            
+            Console.WriteLine(text);
+        }
+    }
+}
