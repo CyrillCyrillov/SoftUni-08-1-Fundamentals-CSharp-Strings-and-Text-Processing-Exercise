9c7d2d67d71c7410d27b8d8125df76f7a3bf3a44
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:32:49 +0200

    Add files via upload

---
+++ Task01_Valid_Usernames/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Task01_Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usersPerCheck = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            foreach (var word in usersPerCheck)
+            {
+                bool isValid = true;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if(word.Length < 3 || word.Length >16)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    
+                    if(char.IsDigit(word[i]) || char.IsLetter(word[i]) || word[i] == '-' || word[i] == '_')
+                    {
+
+                    }
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if(isValid)
+                {
+                    Console.WriteLine(word);
+                }
+                
+            }
+        }
+    }
+}
